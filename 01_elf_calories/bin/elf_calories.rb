f40e2b9c7ad8eea9056c103eebcdcf6177f8f6fb
Devin Roark <droark@invoca.com>
2022-12-01 17:48:28 -0500

    Chomp the newline instead
    and chmod+x

--- 01_elf_calories/bin/elf_calories.rb
+++ 01_elf_calories/bin/elf_calories.rb
@@ -13,7 +13,7 @@ lines = File.readlines(ARGV[0])
 
 lines.each_with_index do |value, line|
   count += value.to_i
-  if value == "\n" || line + 1 == lines.length
+  if value.chomp.empty? || line + 1 == lines.length
     elves.append(count)
     count = 0
   end
